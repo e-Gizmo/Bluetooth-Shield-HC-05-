15816af408e2ef9fd5075fa3ea4289fd799aa801
e-Gizmo <egizmo.mechatronix.central@gmail.com>
2017-05-23 17:45:40 +0800

    Bluetooth Shield (HC-05)

---
+++ BTconfig/BTconfig.ino
@@ -0,0 +1,33 @@
+#include <SoftwareSerial.h>
+
+SoftwareSerial mySerial(2, 3); // RX, TX
+// 2013:3:40964
+
+void setup()  
+{
+  // Open serial communications and wait for port to open:
+  Serial.begin(38400);
+
+  Serial.println("Ready for configuration...");
+
+  // set the data rate for the SoftwareSerial port
+  mySerial.begin(38400);
+}
+
+char a=0;
+char b=0;
+void loop() // run over and over
+{
+  mySerial.listen();
+  if (mySerial.available()){
+    a = mySerial.read();
+    Serial.print(a);
+  }
+
+  if (Serial.available()){
+    b = Serial.read();
+    Serial.print(b);
+    mySerial.print(b);
+  }
+}
+
